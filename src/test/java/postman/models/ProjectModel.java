48a2a05ed4109980a79db72f57ce054f7d8babaf
ntshs <ntshsttxm1997@gmail.com>
2021-07-10 15:57:54 +0300

    Postman, Models xml,json,java Done

---
+++ src/test/java/postman/models/ProjectModel.java
@@ -0,0 +1,19 @@
+package postman.models;
+
+
+    public class ProjectModel {
+    public int id = 22;
+    public String name = "Some_Test2_NatLeb";
+    public String announcement = ":)))";
+    public boolean show_announcement = true;
+    public boolean is_completed = false;
+    public int completed_on;
+    public Integer suite_mode = 3;
+    public String url = "https://aqa06onl01.testrail.io/index.php?/projects/overview/22";
+
+        public ProjectModel() {
+        }
+    }
+
+
+
